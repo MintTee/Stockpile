3f326e9533108cf670a016da332bb8bcdba34d67
mint@tee.com <martin.bost.pro@gmail.com>
2024-12-27 23:58:57 +0100

    Cleanup personal files for github commit

--- test/test.lua
+++
@@ -1,7 +0,0 @@
---local move_item = require("/stockpile/src/move_item")
---local contentdb = require("/stockpile/src/contentdb")
---local logger = require("/stockpile/src/logger")
---local table_utils = require("/stockpile/src/table_utils")
---local data = require("/stockpile/src/data_manager")
---require("/stockpile/var/globals")
---local queue = require("/stockpile/src/queue")
