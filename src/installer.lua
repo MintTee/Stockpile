08800119bf2669e1b96ade2953fc98ebabbd7c07
mint@tee.com <martin.bost.pro@gmail.com>
2024-12-27 23:43:47 +0100

    Yet struggling

--- src/installer.lua
+++ src/installer.lua
@@ -1,16 +1,21 @@
 local files = {
-    "stockpile/config/logger_config.txt",
-    "stockpile/database/content.txt",
-    "stockpile/database/inventories.txt",
-    "stockpile/src/comms.lua",
-    "stockpile/src/contentdb.lua",
-    "stockpile/src/data_manager.lua",
-    "stockpile/src/logger.lua",
-    "stockpile/src/main.lua",
-    "stockpile/src/move_item.lua",
-    "stockpile/src/queue.lua",
-    "stockpile/src/table_utils.lua",
+    "config/logger_config.txt",
+    "database/content.txt",
+    "database/inventories.txt",
+    "src/comms.lua",
+    "src/contentdb.lua",
+    "src/data_manager.lua",
+    "src/logger.lua",
+    "src/main.lua",
+    "src/move_item.lua",
+    "src/queue.lua",
+    "src/table_utils.lua",
+    "var/globals.lua"
+    "var/globals.lua"
+    "LICENSE"
+    "README.md"
 }
+
 local install_success = true
 
 for _, file in ipairs(files) do
@@ -19,7 +24,7 @@ for _, file in ipairs(files) do
     if response then
         local content = response.readAll()
         response.close()
-        local local_file = fs.open(file, "w")
+        local local_file = fs.open("/stockpile/"..file, "w")
         local_file.write(content)
         local_file.close()
         print(file .. " download : Success")
@@ -49,7 +54,7 @@ end
 
 if write_startup then
   print('Stockpile will now run on startup.')
-  io.open('startup', 'w'):write([[shell.run("stockpile/src/main.lua")]]):close()
+  io.open('startup.lua', 'w'):write([[shell.run("stockpile/src/main.lua")]]):close()
 end
 
 if install_success == true then
