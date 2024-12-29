b2a79c598f04bb420bcf4bf41d64593ba72c5c0b
mint@tee.com <martin.bost.pro@gmail.com>
2024-12-29 22:21:36 +0100

    Updated ligic for case if no modem was found.

--- src/main.lua
+++ src/main.lua
@@ -6,8 +6,8 @@ function main()
     term.setCursorPos(1, 1)
     print("[Stockpile initializing...]\n")
     logger("Info", "main", "main function called", "Stockpile initializing... Listening for client command")
-    comms.open_all_modems()
-    comms.wait_for_command()    
+    local opened_modem = comms.open_all_modems()
+    if opened_modem == true then comms.wait_for_command() end    
 end
 
 main()
