401f0cb79c53bc29797fc1e8e49e45915295f074
mint@tee.com <martin.bost.pro@gmail.com>
2025-01-02 15:59:28 +0100

    Simpler logic

--- src/main.lua
+++ src/main.lua
@@ -6,8 +6,7 @@ function main()
     term.setCursorPos(1, 1)
     print("[Stockpile initializing...]\n")
     logger("Info", "main", "main function called", "Stockpile initializing... Listening for client command")
-    local opened_modem = comms.open_all_modems()
-    if opened_modem == true then comms.wait_for_command() end
+    if comms.open_all_modems() == true then comms.wait_for_command() end
 end
 
 main()
