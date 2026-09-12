3489d6beda105bad5fdaad2c689b843df308917d
Martin Bost <martin.bost.pro@gmail.com>
2026-09-12 16:50:28 +0200

    Removed obsolete functions

--- server/src/main.lua
+++ server/src/main.lua
@@ -1,37 +1,8 @@
---[[
-
-//////////////// TO DO LIST ////////////////
-
-- Bug when I scan all invs and update db and then idk why I lose the displayName on the client side for some wierd reasons, when restarting server and loading from bin db, it works again idk why. 
-
-- write super test function to do all API commands at once (from client testing)
-    -get all invs, scan all invs, write db, load db, 
-
-- Test queue functions and see how much they can transfer at once.
-    test max transfer speed (parallel transfers)
-
-- fractionnal db bin in multiple floppy disks for super large storages
-
-]]
-
 local comms = require("/stockpile_server/src/comms")
 local log = require("/stockpile_server/src/log")
 local contentdb = require("/stockpile_server/src/contentdb")
 local bin = require("/stockpile_server/src/bin")
 require("/stockpile_server/var/globals")
-
-local function get_floppy_disks()
-    local disks = {}
-    for _, entry in ipairs(fs.list("/")) do
-        if entry:match("^disk%d*$") then table.insert(disks, "/" .. entry .. "/") end
-    end
-    return disks
-end
-
---obsolete
-local function calculate_capacity(disk_count)
-    return disk_count * 125 / 2 * 1000
-end
 
 local function main()
     term.clear()
